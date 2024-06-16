e0407868dc3db2d0475de0a1023e23b5499594cb
Abhay kumar <mk9416822@gmail.com>
2024-06-17 00:58:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function gray(){
+    const element = document.getElementById('gray')
+    document.body.style.backgroundColor = "gray"
+}
+function white(){
+    const element = document.getElementById('white')
+    document.body.style.backgroundColor = "white"
+    document.body.style.color = "black"
+}
+function blue(){
+    const element = document.getElementById('blue')
+    document.body.style.backgroundColor = "blue"
+}
+function yellow(){
+    const element = document.getElementById('yellow')
+    document.body.style.backgroundColor = "yellow"
+    document.body.style.color = "black"
+
+}
